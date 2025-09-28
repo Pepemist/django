3a8d3808e9cb59615183d435342488c70eb75bd1
Alexey Ponomarev <alex140301@mail.ru>
2025-09-28 22:17:20 +0400

    add html and png

--- test_project/prod/views.py
+++ test_project/prod/views.py
@@ -1,2 +1,34 @@
 from django.shortcuts import render
 
+def index(request):
+    context = {
+        'title': 'Test Title',
+        'is_promotion': False,
+    }
+    return render(request, 'index.html', context)
+
+def products(request):
+    context = {
+        'title': 'Products page',
+        'products': [
+            {
+                'img': '/static/img/1.png',
+                'name': 'first prod',
+                'price': 6060,
+                'desc': 'Первая картинка для теста'
+            },
+            {
+                'img': '/static/img/2.png',
+                'name': '2nd prod',
+                'price': 5000,
+                'desc': 'Вторая картинка для теста'
+            },
+            {
+                'img': '/static/img/3.png',
+                'name': '3rd prod',
+                'price': 6000,
+                'desc': 'Третья картинка для теста'
+            },
+        ]
+    }
+    return render(request, 'prod.html', context)
--- test_project/test_project/urls.py
+++ test_project/test_project/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from prod import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index),
+    path('products', views.products),
+
 ]
