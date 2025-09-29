47c59907b9a3faf6e7982fc7e37b1f41aabec224
Alexey Ponomarev <alex140301@mail.ru>
2025-09-29 13:29:58 +0400

    dumping db

--- test_project/test_project/settings.py
+++ test_project/test_project/settings.py
@@ -117,10 +117,12 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
+STATICFILES_DIRS = (
     BASE_DIR / 'prod/static/',
-]
+)
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
