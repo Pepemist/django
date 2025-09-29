99bdd2248c1fbdab0e184ef6d6a61de563df6cec
Alexey Ponomarev <alex140301@mail.ru>
2025-09-29 10:09:41 +0400

    makemigrations

--- test_project/prod/models.py
+++ test_project/prod/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class ProdCategory(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    desc = models.TextField(null=True, blank=True)
+
+class Product(models.Model):
+    name = models.CharField(max_length=256, unique=True)
+    desc = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=0)
+    image = models.ImageField(upload_to='product_img')
+    category = models.ForeignKey(to=ProdCategory, on_delete=models.CASCADE)
+
